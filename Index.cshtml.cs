760f1c203fac02fbe024f1e2d6f7e8fe39814560
w0771056 <90808231+w0771056@users.noreply.github.com>
2021-10-04 13:21:14 -0400

    change1

--- Index.cshtml.cs
+++ Index.cshtml.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace Rory.Pages
+namespace RoryRomanovich.Pages
 {
     public class IndexModel : PageModel
     {
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Roman
- asp
+ Test
